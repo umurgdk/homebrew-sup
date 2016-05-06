be0c6a0feab9786d194f1803b0668d3df9f5dceb
Umur Gedik <umurgdk@gmail.com>
2016-05-06 07:31:20 +0200

    version bump to v1.1.0

--- Formula/sup.rb
+++ Formula/sup.rb
@@ -1,9 +1,9 @@
 class Sup < Formula
   desc "Configurable changelog generator for git repositories."
   homepage "https://github.com/umurgdk/sup"
-  url "https://github.com/umurgdk/sup/archive/v1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "234994f7621d264df0a1990dae085b951c60fbb9b503c8c060d4f2b1f533c641"
+  url "https://github.com/umurgdk/sup/archive/v1.1.0.tar.gz"
+  version "1.1.0"
+  sha256 "d9f80b8c3368037e1058df698bd3d4451fd5b64bb6d554fc2593454d79e2a5f1"
 
   depends_on "cmake" => :build
   depends_on "boost"
